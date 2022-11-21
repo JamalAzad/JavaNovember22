a7f5c2077eb05b2dec66809ee355ecc92f6267e3
Jamal Azad <102807631+JamalAzad@users.noreply.github.com>
2022-11-21 02:28:39 -0500

    TryMore

---
+++ src/WhileLoopInput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WhileLoopInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input="";
+        while (!input.equals("quit")){
+            System.out.print("Input: ");
+            input= scanner.next().toLowerCase();
+            System.out.println("Your input is: " +input);
+            if (input.equals("quit")){
+                System.out.println("Bye Bye!!");
+            }
+        }
+    }
+}
